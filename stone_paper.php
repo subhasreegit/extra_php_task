e628024421d8d15cd9c2aa0342c1768cd2599ac9
subhasreegit <58703100+subhasreegit@users.noreply.github.com>
2020-02-05 00:28:35 +0530

    Update stone_paper.php

--- stone_paper.php
+++ stone_paper.php
@@ -10,31 +10,18 @@
             
             Choose:
             <br>
+            <br>
             <form action="" method="post"/>
-             <!-- <input type="radio" name="user_choice" class="user_choice" value="Rock" title="Rock" />Rock <br /><br />
-                <input type="radio" name="user_choice" class="user_choice" value="Paper" title="Paper" />Paper <br /><br />
-                <input type="radio" name="user_choice" class="user_choice"  value="Scissors" title="Scissors"/>Scissors<br/><br/>
-                 <input type="submit" name="form_submit" value="Hit"/> -->
                  <button name="user_choice" class="user_choice" value="Rock">Rock</button>
-                 <br>
                  <button name="user_choice" class="user_choice" value="Scissors">Scissors</button>
-                 <br>
                  <button name="user_choice" class="user_choice" value="paper">paper</button>
 
             </form> 
+            <br>
             <?php
                 include 'stone_paper_page2.php';
             ?>
 
         </body>
-        <!-- <script>
-            $(document).ready(function(){
-                $("input[type=radio]").click(function(){
-                     $(".user_choice").submit();
-                     alert("123");
-                 });
-                
-                });
-        </script> -->
 </html>
 
